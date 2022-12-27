b23ee6fa1622d6a11459ee82909ae56bdf830f0c
Jose <jose_level@apple.com>
2022-12-27 11:14:07 -0500

    re-order fake security

--- api_dev/poster/main.py
+++ api_dev/poster/main.py
@@ -18,14 +18,14 @@ def fake_decode_token(token):
     )
 
 
-@app.get("/users/me")
-async def read_users_me(current_user: User = Depends(get_current_user)):
-    return current_user
-
-
 async def get_current_user(token: str = Depends(oauth2_scheme)):
     user = fake_decode_token(token)
     return user
+
+
+@app.get("/users/me")
+async def read_users_me(current_user: User = Depends(get_current_user)):
+    return current_user
 
 
 @app.get("/items/")
