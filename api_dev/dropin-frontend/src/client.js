0119cdf10384b08a76c6793a08145c1ffb3c6aaa
JoseLevel <Jose.levelh@gmail.com>
2023-05-02 20:48:20 -0700

    added token to header for api requests, still some issues

--- api_dev/dropin-frontend/src/client.js
+++ api_dev/dropin-frontend/src/client.js
@@ -1,10 +1,17 @@
 import axios from 'axios';
+import CheckTokenStatus from '../src/helpers/CheckTokenStatus'
 export const apiClient = axios.create({
   baseURL: 'http://127.0.0.1:8000',
   headers: {
     'Content-Type': 'application/json'
   }
 });
+
+// Add Authorization header with bearer token
+if (CheckTokenStatus()) {
+  const token = localStorage.getItem('accessToken');
+  apiClient.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+}
 
 export const loginClient = axios.create({
   baseURL: 'http://127.0.0.1:8000',
--- api_dev/dropin-frontend/src/components/PendingRequests/PedingRequests.jsx
+++ api_dev/dropin-frontend/src/components/PendingRequests/PedingRequests.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {apiClient} from "../../client";
+import useAuthOrRedirect from "../../custom-hooks/useAuthOrRedirect";
+
 
 export default function PendingRequests(){
+    const [users, setUsers] = useState([])
+
+    async function fetchFriendRequests() {
+        try {
+            const userList = await apiClient.get('/friends/requests?')
+            setUsers(userList.data)
+            console.log(users)
+            return userList
+        } catch (e) {
+            console.error(e)
+        }
+    }
+    useAuthOrRedirect("/login")
+    useEffect(() =>{
+        fetchFriendRequests()
+    },[])
     return(
-        <h2>Pending Requests:</h2>
+        <div className="friends-list">
+            <h2>Pending Requests:</h2>
+            <ul>
+                {users.map(user => <li key={user.id}>{user.name}</li>)}
+            </ul>
+        </div>
     )
 }
