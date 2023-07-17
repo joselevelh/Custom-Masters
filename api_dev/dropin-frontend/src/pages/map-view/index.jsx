0c9e0571595d0e52622b62efd893b81d4b177cab
JoseLevel <Jose.levelh@gmail.com>
2023-07-17 16:45:50 -0700

    trying to get mapbox working

--- api_dev/dropin-frontend/src/pages/map-view/index.jsx
+++ api_dev/dropin-frontend/src/pages/map-view/index.jsx
@@ -1,7 +1,8 @@
-import React, {useEffect, useState} from 'react';
-import GoogleMapReact from 'google-map-react';
+import React from 'react';
 import LocationComponent from "../../components/Location/Location";
 import useLocation from "../../custom-hooks/useLocation";
+import Map, {GeolocateControl} from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 
 function MapView() {
@@ -10,50 +11,31 @@ function MapView() {
         <>
             <p>Map View Page</p>
             <LocationComponent/>
-            <SimpleMap location={userLocation}/>
+            <SimpleMap/>
         </>
     );
 }
 
-const AnyReactComponent = ({text}) => <div>{text}</div>;
-
-function SimpleMap({location}) {
-    let defaultProps;
-    if (location) {
-        console.log("Location passed into SimpleMap: "+ location)
-        defaultProps = {
-            center: {
-                lat: location.latitude,
-                lng: location.longitude,
-            },
-            zoom: 11,
-        };
-    } else {
-        defaultProps = {
-            center: {
-                lat: 32.7157,
-                lng: -117.1611,
-            },
-            zoom: 11,
-        };
-    }
-
+function SimpleMap() {
     return (
-        // Important! Always set the container height explicitly
-        <div style={{margin: '0 auto', height: '80vh', width: '60%'}}>
-            <GoogleMapReact
-                bootstrapURLKeys={{key: "AIzaSyBIPjQHY2D5QEGVIqKz_KErsA8Sw-C95Gs"}}
-                defaultCenter={defaultProps.center}
-                defaultZoom={defaultProps.zoom}
+        <div>
+            <Map
+                mapboxAccessToken="pk.eyJ1Ijoiam9zZWZvMTk5NyIsImEiOiJjbGp5emxjYncwMWxhM2dvNDdmaWp5dWFkIn0.NlylS3S5YnEYHQ6JyAR-HA"
+                initialViewState={{
+                    longitude: -100,
+                    latitude: 40,
+                    zoom: 3.5,
+                }}
+                mapStyle="mapbox://styles/mapbox/streets-v11"
             >
-                <AnyReactComponent
-                    lat={32.7157}
-                    lng={-117.1611}
-                    text="My Marker"
+                <GeolocateControl
+                    positionOptions={{enableHighAccuracy: true}}
+                    trackUserLocation={true}
                 />
-            </GoogleMapReact>
+            </Map>
         </div>
     );
 }
+
 
 export default MapView;
