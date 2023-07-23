8542c9a4f7844516f22a0c900ec4655cba7b1397
JoseLevel <Jose.levelh@gmail.com>
2023-07-23 12:13:46 -0700

    fixed current location on mapbox

---
+++ api_dev/dropin-frontend/src/components/Map/SimpleMap.jsx
@@ -0,0 +1,45 @@
+import React, {useEffect, useRef, useState} from "react";
+import mapboxgl from "mapbox-gl";
+
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoiam9zZWZvMTk5NyIsImEiOiJjbGp5emxjYncwMWxhM2dvNDdmaWp5dWFkIn0.NlylS3S5YnEYHQ6JyAR-HA';
+
+export default function SimpleMap({ userLocation }) {
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [lng, setLng] = useState(null); // Initialize with null
+    const [lat, setLat] = useState(null); // Initialize with null
+    const [zoom, setZoom] = useState(11.8);
+
+    useEffect(() => {
+        if (!userLocation.latitude || !userLocation.longitude) return; // Wait for userLocation to be available
+
+        if (!map.current) {
+            map.current = new mapboxgl.Map({
+                container: mapContainer.current,
+                style: 'mapbox://styles/mapbox/streets-v12',
+                center: [userLocation.longitude, userLocation.latitude], // Use user's location here
+                zoom: zoom
+            });
+
+            map.current.on('move', () => {
+                setLng(map.current.getCenter().lng.toFixed(4));
+                setLat(map.current.getCenter().lat.toFixed(4));
+                setZoom(map.current.getZoom().toFixed(2));
+            });
+        }
+    }, [userLocation.latitude, userLocation.longitude, zoom]);
+
+    return (
+        <div align="center">
+            <div className="sidebar">
+                {userLocation.latitude && userLocation.longitude ? (
+                    <>Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}</>
+                ) : (
+                    <>Waiting for user location...</>
+                )}
+            </div>
+            <div ref={mapContainer} className="map-container" />
+        </div>
+    );
+}
--- api_dev/dropin-frontend/src/pages/map-view/index.jsx
+++ api_dev/dropin-frontend/src/pages/map-view/index.jsx
@@ -1,95 +1,15 @@
-// import React, { useRef, useEffect, useState } from 'react';
-// import LocationComponent from "../../components/Location/Location";
-// import useLocation from "../../custom-hooks/useLocation";
-// import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
-//
-// mapboxgl.accessToken = 'pk.eyJ1Ijoiam9zZWZvMTk5NyIsImEiOiJjbGp5emxjYncwMWxhM2dvNDdmaWp5dWFkIn0.NlylS3S5YnEYHQ6JyAR-HA';
-//
-// function MapView() {
-//     return (
-//         <>
-//             <p>Map View Page</p>
-//             <LocationComponent/>
-//             <SimpleMap/>
-//         </>
-//     );
-// }
-//
-// function SimpleMap() {
-//     const mapContainer = useRef(null);
-//     const map = useRef(null);
-//     const [lng, setLng] = useState(-70.9);
-//     const [lat, setLat] = useState(42.35);
-//     const [zoom, setZoom] = useState(9);
-//
-//     useEffect(() => {
-//         if (map.current) return; // initialize map only once
-//         map.current = new mapboxgl.Map({
-//             container: mapContainer.current,
-//             style: 'mapbox://styles/mapbox/streets-v12',
-//             center: [lng, lat],
-//             zoom: zoom
-//         });
-//     })
-//
-//     useEffect(() => {
-//         if (!map.current) return; // wait for map to initialize
-//         map.current.on('move', () => {
-//             setLng(map.current.getCenter().lng.toFixed(4));
-//             setLat(map.current.getCenter().lat.toFixed(4));
-//             setZoom(map.current.getZoom().toFixed(2));
-//         });
-//     });
-//
-//     return (
-//         <div>
-//             <div className="sidebar">
-//                 Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-//             </div>
-//             <div ref={mapContainer} className="map-container"/>
-//         </div>
-//     );
-// }
-//
-//
-// export default SimpleMap;
-import React, { useRef, useEffect, useState } from 'react';
+import React, {useRef, useEffect, useState} from 'react';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import useLocation from "../../custom-hooks/useLocation";
+import SimpleMap from "../../components/Map/SimpleMap";
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoiam9zZWZvMTk5NyIsImEiOiJjbGp5emxjYncwMWxhM2dvNDdmaWp5dWFkIn0.NlylS3S5YnEYHQ6JyAR-HA';
-
-export default function App() {
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng, setLng] = useState(70.9);
-  const [lat, setLat] = useState(42.35);
-  const [zoom, setZoom] = useState(9);
-
-  useEffect(() => {
-    if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v12',
-      center: [lng, lat],
-      zoom: zoom
-    });
-  })
-
-  useEffect(() => {
-    if (!map.current) return; // wait for map to initialize
-    map.current.on('move', () => {
-      setLng(map.current.getCenter().lng.toFixed(4));
-      setLat(map.current.getCenter().lat.toFixed(4));
-      setZoom(map.current.getZoom().toFixed(2));
-    });
-  });
-
-  return (
-    <div align="center">
-      <div className="sidebar">
-        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-      </div>
-      <div ref={mapContainer} className="map-container"  />
-    </div>
-  );
+function MapView() {
+    const userLocation = useLocation();
+    return (
+        <>
+            <SimpleMap userLocation={userLocation}/>
+        </>
+    );
 }
+
+export default MapView;
