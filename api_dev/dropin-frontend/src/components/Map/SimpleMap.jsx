4ced7273e2b05efd26dc5d943c1828944312d0a7
JoseLevel <Jose.levelh@gmail.com>
2023-07-25 17:57:00 -0700

    added dummy pins

--- api_dev/dropin-frontend/src/components/Map/SimpleMap.jsx
+++ api_dev/dropin-frontend/src/components/Map/SimpleMap.jsx
@@ -4,12 +4,13 @@ import mapboxgl from "mapbox-gl";
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoiam9zZWZvMTk5NyIsImEiOiJjbGp5emxjYncwMWxhM2dvNDdmaWp5dWFkIn0.NlylS3S5YnEYHQ6JyAR-HA';
 
-export default function SimpleMap({ userLocation }) {
+export default function SimpleMap({userLocation}) {
     const mapContainer = useRef(null);
     const map = useRef(null);
     const [lng, setLng] = useState(null); // Initialize with null
     const [lat, setLat] = useState(null); // Initialize with null
     const [zoom, setZoom] = useState(11.8);
+    const dummyPins = {"Jake":[-117.15, 32.7,"Jake's Pin"], "Alexa":[-117.2, 32.6,"Alexa's Pin"] }
 
     useEffect(() => {
         if (!userLocation.latitude || !userLocation.longitude) return; // Wait for userLocation to be available
@@ -21,7 +22,14 @@ export default function SimpleMap({ userLocation }) {
                 center: [userLocation.longitude, userLocation.latitude], // Use user's location here
                 zoom: zoom
             });
-
+            for (const pin in dummyPins){
+                const marker = new mapboxgl.Marker({
+                color: "#e1c4ff"
+            })
+                .setLngLat([, 32.7])
+                .setPopup(new mapboxgl.Popup().setHTML("<h1>Hello World!</h1>"))
+                .addTo(map.current);
+            }
             map.current.on('move', () => {
                 setLng(map.current.getCenter().lng.toFixed(4));
                 setLat(map.current.getCenter().lat.toFixed(4));
@@ -29,6 +37,7 @@ export default function SimpleMap({ userLocation }) {
             });
         }
     }, [userLocation.latitude, userLocation.longitude, zoom]);
+
 
     return (
         <div align="center">
@@ -39,7 +48,7 @@ export default function SimpleMap({ userLocation }) {
                     <>Waiting for user location...</>
                 )}
             </div>
-            <div ref={mapContainer} className="map-container" />
+            <div ref={mapContainer} className="map-container"/>
         </div>
     );
 }
